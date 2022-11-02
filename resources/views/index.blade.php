0c0274859937476551bb8fa900d09792e5f90823
Keiko Sato <k-sato@paseli.co.jp>
2022-11-03 01:42:34 +0900

    create index header

---
+++ resources/views/index.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="/css/style.css">
+  <title>Document</title>
+</head>
+<body>
+  <header>
+    <div class="title-block">
+      <h2><a href="#">学習帳</a></h2>
+    </div>
+  </header>
+
+<section>
+  <h3>PHP</h3>
+</section>
+<section>
+  <h3>Laravel</h3>
+</section>
+<section>
+  <h3>SQL</h3>
+</section>
+<section>
+  <h3>Linux</h3>
+</section>
+<section>
+  <h3>NetWork</h3>
+</section>
+</body>
+</html>
