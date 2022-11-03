bfda55bdf42b10011747fc5c55a6e78deae1d544
Keiko Sato <k-sato@paseli.co.jp>
2022-11-04 01:52:20 +0900

    content layout(box shadow)

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,29 +5,32 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="/css/style.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Yomogi&display=swap" rel="stylesheet">
   <title>Document</title>
 </head>
 <body>
   <header>
     <div class="title-block">
-      <h2><a href="#">学習帳</a></h2>
+      <h2><a href="#">エンジニア学習帳</a></h2>
     </div>
   </header>
 
-<section>
+<div class="php-box contents">
   <h3>PHP</h3>
-</section>
-<section>
+</div>
+<div class="laravel-box contents">
   <h3>Laravel</h3>
-</section>
-<section>
+</div>
+<div class="sql-box contents">
   <h3>SQL</h3>
-</section>
-<section>
+</div>
+<div class="linux-box contents">
   <h3>Linux</h3>
-</section>
-<section>
+</div>
+<div class="nw-box contents">
   <h3>NetWork</h3>
-</section>
+</div>
 </body>
 </html>
