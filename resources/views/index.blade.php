8c3752af5b257a886f68898a0081f5737bd4ba41
Keiko Sato <k-sato@paseli.co.jp>
2022-11-04 17:19:27 +0900

    profile edit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -16,21 +16,84 @@
       <h2><a href="#">エンジニア学習帳</a></h2>
     </div>
   </header>
+<section>
+  <div class="php-box contents">
+    <div class="description">
+      <h3>PHPのナレッジ</h3>
+      <ul>
+        <li>phpの関数</li>
+        <li>データベース</li>
+        <li>if文の注意点</li>
+      </ul>
+    </div>
+  </div>
+  <div class="laravel-box contents">
+    <div class="description">
+      <h3>Laravel</h3>
+      <ul>
+        <li>laravelインストール</li>
+        <li>MVC</li>
+        <li>adminLTE</li>
+      </ul>
+    </div>
+  </div>
+  <div class="sql-box contents">
+    <div class="description">
+      <h3>SQL</h3>
+      <ul>
+        <li>phpの関数</li>
+        <li>データベース</li>
+        <li>if文の注意点</li>
+      </ul>
+    </div>
+  </div>
+  <div class="linux-box contents">
+    <div class="description">
+      <h3>Linux</h3>
+      <ul>
+        <li>phpの関数</li>
+        <li>データベース</li>
+        <li>if文の注意点</li>
+      </ul>
+    </div>
+  </div>
+  <div class="nw-box contents">
+    <div class="description">
+      <h3>NetWork</h3>
+      <ul>
+        <li>phpの関数</li>
+        <li>データベース</li>
+        <li>if文の注意点</li>
+      </ul>
+    </div>
+  </div>
+</section>
 
-<div class="php-box contents">
-  <h3>PHP</h3>
-</div>
-<div class="laravel-box contents">
-  <h3>Laravel</h3>
-</div>
-<div class="sql-box contents">
-  <h3>SQL</h3>
-</div>
-<div class="linux-box contents">
-  <h3>Linux</h3>
-</div>
-<div class="nw-box contents">
-  <h3>NetWork</h3>
-</div>
+<section>
+  <div class="profile">
+    <h3><span>Profile</span></h3>
+    <div class="profile-content">
+      <div class="profile-top">
+        <img src="/images/arashi_chan_cut.png">
+        <p>no name</p>
+      </div>
+      <div class="profile-body">
+        <p>趣味:ペットのワンコを愛でる、推し事、麻雀、読書、プログラミング学習</p>
+        <p>３０代からエンジニアとしてスタート。日々プログラミングの勉強中。早く一人前になりたい。</p>
+        <ul>
+          <li class="btn">portfolio ></li>
+          <li class="btn">Garally ></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</section>
+
+  <footer>
+    <ul>
+      <li>CONTACT</li>
+      <li>SNS</li>
+    </ul>
+  </footer>
 </body>
 </html>
