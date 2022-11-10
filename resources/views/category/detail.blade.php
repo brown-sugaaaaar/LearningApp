f4f1f07d1bf4859a723826c2da18987e42c22bdf
SatoKeiko <k-sato@paseli.co.jp>
2022-11-10 13:10:14 +0900

    add include header,footer

--- resources/views/category/detail.blade.php
+++ resources/views/category/detail.blade.php
@@ -1,30 +1,13 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="/css/style.css">
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Yomogi&display=swap" rel="stylesheet">
-  <title>Document</title>
-</head>
-<body>
-  <header>
-    <div class="title-block">
-      <h2><a href="/">エンジニア学習帳</a></h2>
-    </div>
-  </header>
+
+@include('layouts.header')
 
 <section>
-    <h2>PHP Post detail</h2>
+    <h2>{{ $post->title }}</h2>
 
     <div class="post-list">
       <ul>
         <li>
           <div>
-            <p>{{ $post->title }}</p>
             <p>{{ $post->content }}</p>
             <p>{{ $post->category->name }}</p>
             <p>{{ $post->created_at }}</p>
@@ -34,11 +17,4 @@
     </div>
 </section>
 
-  <footer>
-    <ul>
-      <li>CONTACT</li>
-      <li>SNS</li>
-    </ul>
-  </footer>
-</body>
-</html>
+@include('layouts.footer')
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,8 @@
+<footer>
+    <ul>
+      <li>CONTACT</li>
+      <li>SNS</li>
+    </ul>
+  </footer>
+</body>
+</html>
