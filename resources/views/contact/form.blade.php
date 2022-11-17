dfbf8a9817634841f391a60822c0b917b9a2052d
Keiko Sato <k-sato@paseli.co.jp>
2022-11-17 23:31:11 +0900

    add form css

--- resources/views/contact/form.blade.php
+++ resources/views/contact/form.blade.php
@@ -2,31 +2,32 @@
 
 <form id="form">
 
-  <h2>Contact Form</h2>
+  <h2 class="form-title">Contact Form</h2>
 
-  <div>
+  <div class="form-all">
     <div class="col-sm-4">
-        <label class="form-label" for="name">name</label>
-        <input type="text" class="form-control" id="name">
+      <label class="form-label" for="name">name</label>
+      <input type="text" class="form-control" id="name">
     </div>
 
     <div class="col-sm-4">
-        <label class="form-label" for="email">email</label>
-        <input type="email" class="form-control" id="email" placeholder="hogehoge@example.com">
+      <label class="form-label" for="email">email</label>
+      <input type="email" class="form-control" id="email" placeholder="hogehoge@example.com">
     </div>
 
     <div class="col-sm-8">
-        <label class="form-label" for="subject">subject</label>
-        <input type="text" class="form-control" id="subject">
+      <label class="form-label" for="subject">subject</label>
+      <input type="text" class="form-control" id="subject">
     </div>
 
-    <div class="col-sm-8">
-        <label class="form-label" for="inquiry">inquiry</label>
-        <textarea class="form-control" rows="10" id="inquiry"></textarea>
+    <div class="col-sm-12">
+      <label class="form-label" for="inquiry">inquiry</label>
+      <textarea class="form-control" rows="10" id="inquiry"></textarea>
     </div>
+
+    <button type="submit" class="btn btn-primary">送信</button>
   </div>
 
-  <button type="submit" class="btn btn-primary">送信</button>
 
 </form>
 
--- resources/views/layouts/form-header.blade.php
+++ resources/views/layouts/form-header.blade.php
@@ -5,16 +5,16 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link rel="stylesheet" href="/css/form.css">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Yomogi&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="/css/form.css">
 
   <title>Contact Form</title>
 </head>
 <body>
   <header>
     <div class="title-block">
-      <h2><a href="/" class="text-decoration-none link-dark display-4 font-weight-bold">エンジニア学習帳</a></h2>
+      <h2 class="display-3 font-weight-bold"><a href="/" class="text-decoration-none link-dark">エンジニア学習帳</a></h2>
     </div>
   </header>
