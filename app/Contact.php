da6b5e5b9772efe275ae81d51b8eff18c0197854
SatoKeiko <k-sato@paseli.co.jp>
2022-12-06 16:22:21 +0900

    add edit_phase_flag

--- app/Contact.php
+++ app/Contact.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Contact extends Model
 {
-    protected $fillable = ['subject','inquiry','email','name']; //保存したいカラム名が複数の場合
+    protected $fillable = ['subject','inquiry','email','name','phase_flag']; //保存したいカラム名が複数の場合
 }
--- resources/views/admin/contacts/edit.blade.php
+++ resources/views/admin/contacts/edit.blade.php
@@ -30,21 +30,30 @@
                 <div class="col-sm-12">
                     <div class="from-group">
                         <label>inquiry</label>
-                        <input type="text" class="form-control" name="inquiry"  value="{{$contact->inquiry}}">
+                        <input type="text" class="form-control" name="inquiry" value="{{$contact->inquiry}}">
                     </div>
                 </div>
 
                 <div class="col-sm-12">
                     <div class="from-group">
                         <label>name</label>
-                        <input type="text" class="form-control" name="inquiry"  value="{{$contact->name}}">
+                        <input type="text" class="form-control" name="inquiry" value="{{$contact->name}}">
                     </div>
                 </div>
 
                 <div class="col-sm-12">
                     <div class="from-group">
                         <label>email</label>
-                        <input type="text" class="form-control" name="inquiry"  value="{{$contact->email}}">
+                        <input type="text" class="form-control" name="inquiry" value="{{$contact->email}}">
+                    </div>
+                </div>
+
+                <div class="col-sm-8">
+                    <div class="from-group" style="display:flex;">
+                        <label>phase</label>
+                        <input type="checkbox" class="form-control" name="phase_flag" value="0" @if($contact->phase_flag == 0) checked @endif>0
+                        <input type="checkbox" class="form-control" name="phase_flag" value="1" @if($contact->phase_flag == 1) checked @endif>1
+                        <input type="checkbox" class="form-control" name="phase_flag" value="2" @if($contact->phase_flag == 2) checked @endif>2
                     </div>
                 </div>
             </div>
